90e7b591ef37a2403bc7a09ba72c7fd5741d2afc
shoaib-khalid <shoaib@kalsym.com>
2023-12-08 16:18:12 +0500

    Testing basic HTTP request

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # hello-rust
 Repository for basic rust examples
+
+
+Basic HTTP request test
+
+Output:
+
+Sending request to uri: [http://httpbin.org/ip]
+
+Response: 200 OK
+{
+  "origin": "202.69.61.146"
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+extern crate futures;
+extern crate hyper;
+extern crate tokio_core;
+
+use std::io::{self, Write};
+use futures::{Future, Stream};
+use hyper::{Client, Uri};
+use tokio_core::reactor::Core;
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut core = Core::new().unwrap();
+    let client = Client::new(&core.handle());
+
+    let uri: Uri = "http://httpbin.org/ip".parse().unwrap();
+    println!("Sending request to uri: [{}]", uri);
+    println!();
+
+    let work =
+        client.get(uri).and_then(|res| {
+            println!("Response: {}", res.status());
+
+            res.body().for_each(|chunk| {
+                io::stdout()
+                    .write_all(&chunk)
+                    .map_err(From::from)
+            })
+        });
+    core.run(work).unwrap();
 }
+
+// fn main() {
+//     println!("Hello, world!");
+// }
+
