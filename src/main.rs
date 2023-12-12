ccaab594c4002cad1c8de6f3d2853af58bb3b340
shoaib-khalid <shoaib@kalsym.com>
2023-12-12 15:40:38 +0500

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+
+struct Person {
+    name : String,
+    age : u8,
+}
+
+impl Person {
+    // This is a method. It has access to the instance of the struct.
+    fn greet(&self){
+        println!("Hello my name is: {}, and my age is: {}", self.name, self.age);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let person= Person {
+        name : String :: from("Asim"),
+        age : 38,
+    };
+
+
+    println!("Hello Person, world!");
 }
