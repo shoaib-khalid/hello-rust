0fcad8e1bffb69193ed1e4b1597edd4f2539d44c
shoaib-khalid <shoaib@kalsym.com>
2023-12-23 14:41:09 +0500

    added factorial method

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
+    println! ("Sum of {} and {} is {}",5,6,sum(5,6));
+}
+
+fn sum(num1: i32, num2: i32) -> i32 {
+        num1 + num2
+}
+
+
+fn factorial (num: i32) -> i32 {
+        let mut result:i32 = 1;
+        for i in 1..=num {
+            result *= i;
+        }
+        return result;
 }
